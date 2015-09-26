5cfab14b55f736112b23159457dac67d91afff2f
Eirielson <eirielson@gmail.com>
2015-09-26 11:16:10 -0300

    Script baseCharacter criado com inicio da state machine para animação

---
+++ Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacter : MonoBehaviour {
+
+	/// Enum para o state machine do character
+	enum State{
+		idle,
+		walk,
+		run,
+		attack,
+		hit,
+		die
+	}
+
+	public AnimationClip idle;
+	public AnimationClip walk;
+	public AnimationClip run;
+	public AnimationClip attack;
+	public AnimationClip hit;
+	public AnimationClip die;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
