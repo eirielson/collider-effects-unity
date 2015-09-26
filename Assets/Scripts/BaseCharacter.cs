628a0b2a4c6137282bef830b4055ae063bcb41eb
Eirielson <eirielson@gmail.com>
2015-09-26 14:21:18 -0300

    Adicionadno CrossFade na transicao de alguns estados

--- Assets/Scripts/BaseCharacter.cs
+++ Assets/Scripts/BaseCharacter.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BaseCharacter : MonoBehaviour {
 
 	/// Enum para o state machine do character
-	enum State{
+	public enum State{
 		idle,
 		walk,
 		run,
@@ -21,7 +21,9 @@ public class BaseCharacter : MonoBehaviour {
 	public AnimationClip die;
 
 
-	#region
+	public State actualState;
+	public State nextState;
+	#region characterVars
 	float speed = 1.0f;
 	float verticalSpeed = 0.0f; //total velocidade em x
 	float horizontalSpeed = 0.0f; // velocidade em Y
@@ -30,19 +32,24 @@ public class BaseCharacter : MonoBehaviour {
 	public float runSpeed = 5.0f;
 	bool isHit;
 	bool isDead;
-		
+	bool isRunning;		
 	CharacterController m_characterController;
+	#endregion characterVars
 
-	#endregion
 	/// <summary>
 	/// Um passo anterior ao Start. Prepara o objeto para estar pronto no método start
 	/// </summary>
 	void Awake(){
+		m_characterController = gameObject.AddComponent<CharacterController> ();
 
+		m_characterController.center = new Vector3 (0.0f, 0.62f, 0.0f);
+		m_characterController.radius = 0.35f;
+		m_characterController.height = 1.21f;
 	}
 
 
 	// Use this for initialization
+	// x 0.02  0.6,0 
 	void Start () {
 		walk.wrapMode = WrapMode.PingPong;
 	}
@@ -52,13 +59,44 @@ public class BaseCharacter : MonoBehaviour {
 	
 	}
 
-	public void SetState(){
+	public virtual	void Move(){
+		if (m_characterController.isGrounded) { //se o personagem está sobre um collider.
+			transform.Rotate(0,horizontalSpeed,0);
+			Vector3 forward = transform.TransformDirection(Vector3.forward); // obtem a direção do transform. Obtem o Z dele (para frente)
 
+			m_characterController.SimpleMove(forward * verticalSpeed);
+		}
+	}
+
+	public void SetState(State state){
+		actualState = state;
+	}
+
+
+	public void SetAnimationState(){
+		if (actualState != State.hit && actualState != State.die) {
+
+			if(actualState == State.walk){
+				animation.CrossFade(walk.name,0.25f);  // faz a transição de uma animação para outra em determinado tempo.
+			}else if (actualState == State.run) {
+				animation.CrossFade(run.name, 0.25f);
+			}else if (actualState == State.idle) {
+				animation.CrossFade(idle.name, 0.25f);
+			}
+
+		}
 	}
 
 
 	public void OnGUI(){
-		speed = GUILayout.HorizontalSlider (speed, -10.0f, 10.0f);
+		if (GUILayout.Button ("State Walk")) {
+			SetState(State.walk);
+		}
+		if (GUILayout.Button ("State Run")) {
+			SetState(State.run);
+		}
+
+		/*speed = GUILayout.HorizontalSlider (speed, -10.0f, 10.0f);
 
 		foreach (AnimationState a in animation) {
 			if(GUILayout.Button(a.name)){
@@ -66,6 +104,8 @@ public class BaseCharacter : MonoBehaviour {
 			}
 			a.speed = speed;
 		}
+		*/
+
 	}
 
 }
