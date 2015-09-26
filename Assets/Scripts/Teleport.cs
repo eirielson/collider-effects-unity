62720a682785eff15ae3f8f3183416e00b2ac6fc
Eirielson <eirielson@gmail.com>
2015-09-26 09:50:16 -0300

    Script do evento OnTrigger criado e manipulação do objeto de colisão

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public Transform teleportTo;
+
+	public void OnTriggerEnter(Collider other){
+		Debug.Log (other.gameObject.name);
+		other.transform.position = teleportTo.position;
+	}
+
+}
